71605bc6a16b4027a53890c10077b7acd01387eb
MykhailenkoAnton <mykhailencko2594@gmail.com>
2021-11-25 13:27:38 +0200

    G6l3 new

--- Tests/G6l3/main.cpp
+++ Tests/G6l3/main.cpp
@@ -5,28 +5,38 @@ int main()
 
     menu();
     char ch;
-    std::cin >> ch;
-    while ((ch != 'a' && ch != 'A') && (ch != 'b' && ch != 'B') && (ch != 'c' && ch != 'C') && (ch != 'd' && ch != 'D'))
+    bool end = false;
+    while (!end)
     {
-        std::cout << "enter please a b c d: ";
         std::cin >> ch;
+            switch (ch)
+        {
+        case 'a':
+        case 'A': 
+            std::cout << "AAAAAAAAA!!!\n";
+            end = true;
+            break;
+        case 'b':
+        case 'B':
+            std::cout << "BBBBBBBBBB!!!\n";
+            end = true;
+            break;
+        case 'c':
+        case 'C':
+            std::cout << "CCCCCCCCCC!!!\n";
+            break;
+            end = true;
+        case 'd':
+        case 'D':
+            std::cout << "CCCCCCCCCC!!!\n";
+            break;
+        default:
+            std::cout << "enter please a b c d: ";
+            end = true;
+            break;
+        }
     }
-    switch (ch)
-    {
-    case 'a':
-    case 'A': std::cout << "AAAAAAAAA!!!\n";
-        break;
-    case 'b':
-    case 'B': std::cout << "BBBBBBBBBB!!!\n";
-        break;
-    case 'c':
-    case 'C': std::cout << "CCCCCCCCCC!!!\n";
-        break;
-    case 'd':
-    case 'D': std::cout << "CCCCCCCCCC!!!\n";
-    default:
-        break;
-    }
+    
     
     return 0;
 }
