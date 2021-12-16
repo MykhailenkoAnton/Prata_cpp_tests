d1c3cf4b9b9feaeb537be54dcb0033a4275f65a6
MykhailenkoAnton <Mykhailencko2594@gmail.com>
2021-12-16 22:54:25 +0200

    g7l10

--- Tests/G7l10/main.cpp
+++ Tests/G7l10/main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 double dbs(double d, double x);
 double dba(double, double);
-double calculate(double x, double y, double (*db[]) (double, double));
+void calculate(double x, double y, double (*db[]) (double, double));
+// double (*pf[2])(double, double);
+
 int main()
 {
     std::cout << "Enter please 2 numbers: ";
     double nm1, nm2;
-    
+    double (*pf[2])(double, double) = {dbs, dba};
     while (!(std::cin >> nm1 >> nm2))
     {
         std::cin.clear();
@@ -15,20 +17,28 @@ int main()
             continue;
         }
         std::cout << "Bad input, try again: ";
+
     }
-    std::cout << "Sum = " <<  calculate(nm1, nm2, dbs) << std::endl;
-    std::cout << "Avarage sum = " << calculate(nm1, nm2, dba) << std::endl;
+    
+    calculate(nm1, nm2, pf);
+    // std::cout << "Avarage sum = " << calculate(nm1, nm2, dba) << std::endl;
+    
     return 0;
 }
 double dbs(double a, double b)
 {
     return a + b;
 }
-double calculate(double x, double y, double (*db)(double, double))
+void calculate(double x, double y, double (*db[])(double, double))
 {
-    std::cout << "U rezult is: ";
-    double rezult = (*db)(x, y);
-    return rezult;
+    // std::cout << "U rezult is: ";
+    // double rezult = (*db)(x, y);
+    // return rezult;
+    for (int i = 0; i < 2; i++)
+    {
+        std::cout << db[i](x, y) << std::endl;
+    }
+    
 }
 double dba (double z, double r)
 {
@@ -36,3 +46,4 @@ double dba (double z, double r)
     avarage = (z + r) / 2;
     return avarage;
 }
+
