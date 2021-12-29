85a62c16b6bd20729cf398764752d1e68488b3c2
MykhailenkoAnton <Mykhailencko2594@gmail.com>
2021-12-30 00:43:31 +0200

    8.12Q

--- Tests/G8Q/main.cpp
+++ Tests/G8Q/main.cpp
@@ -309,101 +309,191 @@
 //     }
 //     os.setf(initial);
 // }
-8.9
-const int ArSize = 80;
-char * legt(const char * str, int n = 1);
-int main()
-{
-    char Sample[ArSize];
-    std::cout << "Enter a string: ";
-    std::cin.get(Sample, ArSize);
-    char * ps = legt(Sample, 4);
-    std::cout << ps << std::endl;
-    delete [] ps;
-    ps = legt(Sample);
-    std::cout << ps << std::endl;
-    delete [] ps;
-    return 0;
-}
-char * legt(const char * str, int n)
-{
-    if (n < 0)
-    {
-        n = 0;
-    }
-    char * p = new char[n + 1];
-    int i;
-    for (i = 0; i < n && str[i]; i++)
-    {
-        p[i] = str[i];
-    }
-    while (i <= n)
-    {
-        p[i++] = '\0';
-    }
-    return p;
-}
+// 8.9
+// const int ArSize = 80;
+// char * legt(const char * str, int n = 1);
+// int main()
+// {
+//     char Sample[ArSize];
+//     std::cout << "Enter a string: ";
+//     std::cin.get(Sample, ArSize);
+//     char * ps = legt(Sample, 4);
+//     std::cout << ps << std::endl;
+//     delete [] ps;
+//     ps = legt(Sample);
+//     std::cout << ps << std::endl;
+//     delete [] ps;
+//     return 0;
+// }
+// char * legt(const char * str, int n)
+// {
+//     if (n < 0)
+//     {
+//         n = 0;
+//     }
+//     char * p = new char[n + 1];
+//     int i;
+//     for (i = 0; i < n && str[i]; i++)
+//     {
+//         p[i] = str[i];
+//     }
+//     while (i <= n)
+//     {
+//         p[i++] = '\0';
+//     }
+//     return p;
+// }
 
 //8.10
-unsigned long left(unsigned long num, unsigned ct);
-char * left(const char * str, int n = 1);
+// unsigned long left(unsigned long num, unsigned ct);
+// char * left(const char * str, int n = 1);
+// int main()
+// {
+//     char * trip = "Hawaii!!";
+//     unsigned long n = 44444444;
+//     int i;
+//     char * tepm;
+//     for (int i = 1; i < 10; i++)
+//     {
+//         std::cout << left(n, i) << std::endl;
+//         tepm = left(trip, i);
+//         std::cout << tepm << std::endl;
+//         delete [] tepm;
+//     }
+//     return 0;
+// }
+// unsigned long left(unsigned long num, unsigned ct)
+// {
+//     unsigned digits = 1;
+//     unsigned long n = num;
+//     if (ct == 0 || num == 0)
+//     {
+//         return 0;
+//     }
+//     while (n /= 10)
+//     {
+//         digits++;
+//     }
+//     if (digits > ct)
+//     {
+//         ct = digits - ct;
+//         while (ct--)
+//         {
+//             num /= 10;
+//         }
+//         return num;
+//     }
+//     else
+//     {
+//         return num;
+//     }
+// }
+// char * left(const char * str, int n)
+// {
+//     if (n < 0)
+//     {
+//         return 0;
+//     }
+//     char * p = new char[n + 1];
+//     int i;
+//     for (i = 0; i < n && str[i]; i++)
+//     {
+//         p[i] = str[i];
+//     }
+//     while (i <= n)
+//     {
+//         p[i] = '\0';
+//         i++;
+//     }
+//     return p;
+// }
+
+// 8.11
+// template <typename T>
+// void Show(T &a, T &b);
+// int main()
+// {
+//     int i = 10;
+//     int j = 20;
+//     std::cout << "i, j = " << i << ", " << j << "\n";
+//     std::cout << "Using compiler-generated int swapper:\n";
+//     Show(i, j);
+//     std::cout << "Now i, j = " << i <<", " << j << ".\n";
+//     double x = 24.5;
+//     double y = 81.7;
+//     std::cout << "x, у = " << x << ", " << y << ".\n";
+//     std::cout << "Using compiler-generated double swapper:\n";
+//     Show(x, y);
+//     std::cout << "Now x, y = " << x <<", " << y << ".\n";
+//     return 0;
+// }
+// template <typename T>
+// void Show(T &a, T &b)
+// {
+//     T temp;
+//     temp = a;
+//     a = b;
+//     b = temp;
+// }
+
+//8.12
+// template <typename T>
+// void Swap(T &a, T &b);
+
+// template <typename T>
+// void Swap(T *a, T *b, int);
+// void Show(int a[]);
+
+// const int Lim = 8;
+// int main()
+// {
+//     int i = 10, j = 20;
+//     std::cout << "i, j = " << i << ", " << j << ".\n";
+//     std::cout << "Using compiler-generated int swapper:\n";
+//     Swap(i, j);
+//     std::cout << "Now i, j = " << i << ", " << j << ".\n";
+//     int d1[Lim] = {1, 1, 1, 1, 1, 1, 1, 1};
+//     int d2[Lim] = {5, 4, 6, 7, 8, 2, 6, 3};
+//     Show(d1);
+//     Show(d2);
+//     Swap(d1, d2, Lim);
+//     std::cout << "Swapped arrays:\n";
+//     Show(d1);
+//     Show(d2);
+//     return 0;
+// }
+// template <typename T>
+// void Swap(T &a, T &b)
+// {
+//     T temp;
+//     temp = a;
+//     a = b;
+//     b = temp;
+// }
+// template <typename T>
+// void Swap(T *a, T *b, int n)
+// {
+//     T temp;
+//     for (int  i = 0; i < n; i++)
+//     {
+//         temp = a[i];
+//         a[i] = b[i];
+//         b[i] = temp;
+//     }
+// }
+// void Show(int a[])
+// {
+//     std::cout << a[0] << a[1] << "/";
+//     std::cout << a[2] << a[3] << "/";
+//     for (int i = 4; i < Lim; i++)
+//     {
+//         std::cout << a[i];
+//     }
+//     std::cout << std::endl;
+// }
+
+//8.13
 int main()
 {
-    char * trip = "Hawaii!!";
-    unsigned long n = 44444444;
-    int i;
-    char * tepm;
-    for (int i = 1; i < 10; i++)
-    {
-        std::cout << left(n, i) << std::endl;
-        tepm = left(trip, i);
-        std::cout << tepm << std::endl;
-        delete [] tepm;
-    }
     return 0;
-}
-unsigned long left(unsigned long num, unsigned ct)
-{
-    unsigned digits = 1;
-    unsigned long n = num;
-    if (ct == 0 || num == 0)
-    {
-        return 0;
-    }
-    while (n /= 10)
-    {
-        digits++;
-    }
-    if (digits > ct)
-    {
-        ct = digits - ct;
-        while (ct--)
-        {
-            num /= 10;
-        }
-        return num;
-    }
-    else
-    {
-        return num;
-    }
-}
-char * left(const char * str, int n)
-{
-    if (n < 0)
-    {
-        return 0;
-    }
-    char * p = new char[n + 1];
-    int i;
-    for (i = 0; i < n && str[i]; i++)
-    {
-        p[i] = str[i];
-    }
-    while (i <= n)
-    {
-        p[i] = '\0';
-        i++;
-    }
-    return p;
 }
