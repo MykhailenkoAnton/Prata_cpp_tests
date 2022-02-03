78a6c4d4e7020b0afac607402d0146707b57c238
MykhailenkoAnton <Mykhailencko2594@gmail.com>
2022-02-04 01:10:13 +0200

    Array Stock

---
+++ Tests/G10/stock20.h
@@ -0,0 +1,23 @@
+#ifndef STOCK20_H_
+#define STOCK20_H_
+#include <string>
+class Stock
+{
+private:
+    std::string company;
+    double shares;
+    double share_val;
+    double total_val;
+public:
+    Stock();
+    Stock::Stock(const std::string & co, long n, double pr);
+    ~Stock();
+    void buy(long num, double price);
+    void sell(long num, double price);
+    void update(double price);
+    void show() const;
+    void set_tot();
+    const Stock & topval(const Stock & s) const;
+};
+
+#endif
