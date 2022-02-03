e42cee981db88358ec5b097bd25d1b4b43ea4298
MykhailenkoAnton <Mykhailencko2594@gmail.com>
2022-02-03 23:46:16 +0200

    G10

--- Tests/G10/usestock.cpp
+++ Tests/G10/usestock.cpp
@@ -15,31 +15,35 @@ int main()
     // fluffy_the_cat.show();
 
     // + конструктор и дестркутор
-    Stock * s;
-    std::cout << "Using constructors to create new objects\n";
+    {
+        Stock * s;
+        std::cout << "Using constructors to create new objects\n";
 
-    Stock stock1("NanoSmart", 12, 20.0); // первый синтаксис
-    s = new Stock("NanoSmart", 12, 20.0);
+        Stock stock1("NanoSmart", 12, 20.0); // первый синтаксис
+        s = new Stock("NanoSmart", 12, 20.0);
 
-    stock1.show();
-    Stock stock2 = Stock("Bobo object", 2, 2.0); // второй синтаксис
-    stock2.show();
+        stock1.show();
+        Stock stock2 = Stock("Bobo object", 2, 2.0); // второй синтаксис
+        stock2.show();
 
-    delete s;
-    // Присваивание stockl объекту stock2
-    std::cout << "Assigning stockl to stock2:\n";
-    stock2 = stock1;
+        delete s;
+        // Присваивание stockl объекту stock2
+        std::cout << "Assigning stockl to stock2:\n";
+        stock2 = stock1;
 
-    // Вывод stockl и stock2
-    std::cout << "Listing stockl and stock2:\n";
-    stock1.show();
-    stock2.show();
+        // Вывод stockl и stock2
+        std::cout << "Listing stockl and stock2:\n";
+        stock1.show();
+        stock2.show();
 
-    // Использование конструктора для сброса объекта
-    std::cout << "Using a constructor to reset an object\n";
-    stock1 = Stock("Nighty Foods", 10, 50.0);
-    std::cout << "Revised stockl:\n";
-    stock1.show();
-    std::cout << "Done!\n";
+        // Использование конструктора для сброса объекта
+        std::cout << "Using a constructor to reset an object\n";
+        stock1 = Stock("Nighty Foods", 10, 50.0);
+        std::cout << "Revised stockl:\n";
+        stock1.show();
+        std::cout << "Done!\n";
+    }
+
+
     return 0;
 }
