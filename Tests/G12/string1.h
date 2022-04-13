6c04550da2ef3d977d8c4d59e58b2d9f855f691e
MykhailenkoAnton <mykhailencko2594@gmail.com>
2022-04-13 15:09:50 +0300

    G12!

--- Tests/G12/string1.h
+++ Tests/G12/string1.h
@@ -5,9 +5,20 @@
 class NString
 {
 private:
-    
+    char * str;
+    int len;
+    static int num_strings;
+    static const int CINLIM = 80;
 public:
- 
+    // Конструкторы и другие методы
+    NString(const char * s); // конструктор
+    NString(); // конструктор по умолчанию
+    NString(const NString & s); // конструктор копирования
+    ~NString(); // деструктор
+    int lenght() const {return len;}
+
+    // Методы перегруженных операций
+    
 };
 
 
