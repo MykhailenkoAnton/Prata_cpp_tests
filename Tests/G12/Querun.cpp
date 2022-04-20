140cbbca5f22d73a1203e4556bf05c5c8db14bdf
MykhailenkoAnton <mykhailencko2594@gmail.com>
2022-04-21 00:19:58 +0300

    QUEUE

---
+++ Tests/G12/Querun.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "Queue.h"
+const int MIN_PER_HR = 60;
+
+bool newcostumer(double x); // появился ли новый клиент?
+
+int main()
+{
+    using std::ios_base;
+    srand(time(0));
+    std::cout << "Case Study: Bank of Heather Automatic Teller\n";
+    std::cout << "Enter maximum size of queue: "; // ввод максимального размера очереди
+    int qs;
+    std::cin >> qs;
+    Queue line(qs); // очередь может содержать до qs людей
+    std::cout << "Enter the number of simulation hours: "; // ввод количества эмулируемых часов
+    int hours;
+    std::cin >> hours; // часы эмуляции
+    // Эмуляция будет запускать один цикл в минуту
+    long cyclelimit = MIN_PER_HR * hours; // количество циклов
+    std::cout << "Enter the average number of customers per hour: ";
+                        // Ввод количества клиентов в час
+    double perhour; // среднее количество появлений за час
+    std::cin >> perhour;
+    double min_per_cast = MIN_PER_HR / perhour; // среднее время между появлениями
+    Item temp; // данные нового клиента
+    long turnaway = 0; // не допущены в полную очередь
+    long customers = 0; // присоединены к очереди
+    long served = 0; // обслужены во время эмуляции
+    long sum_line = 0; // общая длина очереди
+    int wait_time = 0; // время до освобождения банкомата
+    long line_wait = 0; // общее время в очереди
+
+    // Запуск моделирования
+    for (int cycle = 0; cycle < cyclelimit; cycle++)
+    {
+        if (newcostumer(min_per_cast)) // есть подошедший клиент
+        {
+            if (line.isfull())
+            {
+                turnaway++;
+            }
+            else
+            {
+                customers++;
+                temp.set(cycle); // cycle = время прибытия
+                line.enqueue(temp); // добавление новичка в очередь
+            }
+        }
+        if(wait_time <= 0 && !line.isempty())
+        {
+            line.dequeue(temp); // обслуживание следующего клиента
+            wait_time = temp.ptime();
+            line_wait += cycle - temp.when();
+            served++;
+        }
+        if (wait_time > 0)
+        {
+            wait_time--;
+        }
+        sum_line += line.queuecount();
+    }
+    // Вывод результатов
+    if (customers > 0)
+    {
+        std::cout << "customers accepted: " << customers << std::endl; // принято клиентов
+        std::cout << " customers served: " << served << std::endl; // обслужено клиентов
+        std::cout << " turnaways: " << turnaway << std::endl; // не принято клиентов
+        std::cout << "average queue size: "; // средний размер очереди
+        std::cout.precision(2);
+        std::cout.setf(ios_base::fixed, ios_base::floatfield);
+        std::cout << (double) sum_line / cyclelimit << std::endl;
+        std::cout << " average wait time: " // среднее время ожидания (минут)
+                    << (double) line_wait / served << " minutes" << std::endl;
+    }
+    else
+    {
+        std::cout << "No customers\n";
+    }
+    std::cout << "Done!\n";
+    return 0;
+}
+// x = среднее время в минутах между клиентами
+// возвращается значение true, если в эту минуту появляется клиент
+bool newcostumer(double x)
+{
+    return (rand() * x / RAND_MAX < 1); 
+}
--- Tests/G12/Queue.cpp
+++ Tests/G12/Queue.cpp
@@ -31,3 +31,52 @@ int Queue::queuecount() const
 {
     return items;
 }
+// Добавление элемента в очередь
+bool Queue::enqueue(const Item & item)
+{
+    if (isfull())
+    {
+        return false;
+    }
+    Node * add = new Node; // создание узла
+    // При неудачном выполнении операция new генерирует исключение std::bad_alloc
+    add->item = item; // занесение указателей на узлы
+    add->next = NULL;
+    items++;
+    if (front == NULL) // если очередь пуста,
+    {
+        front = add; // элемент помещается в начало
+    }
+    else
+    {
+        rear->next = add; // иначе он помещается в конец
+    }
+    rear = add;
+    return true;
+}
+// Помещение элемента front в переменную item и его удаление из очереди
+bool Queue::dequeue(Item & item)
+{
+    if (front == NULL)
+    {
+        return false;
+    }
+    item = front->item;
+    items--;
+    Node * temp = front;
+    front = front->next;
+    delete temp;
+    if (items == 0)
+    {
+        rear = 0;
+    }
+    return true;
+}
+// Метод класса Customer
+// При появлении клиента фиксируется момент его прибытия, а время
+// обслуживания выбирается случайным образом из диапазона 1-3
+void Costumer::set(long when)
+{
+    processtime = rand() % 3 + 1;
+    arrive = when;  
+}
