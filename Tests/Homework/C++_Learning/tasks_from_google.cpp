1aa1b12ee6cc4b016164eda30534ce7bb5050a8c
MykhailenkoAnton <mykhailencko2594@gmail.com>
2022-07-26 22:43:43 +0300

    +

--- Tests/Homework/C++_Learning/tasks_from_google.cpp
+++ Tests/Homework/C++_Learning/tasks_from_google.cpp
@@ -568,32 +568,37 @@
 //     return 0;
 // }
 
+// #include <iostream>
+// #include <thread>
+// #include <chrono>
+// #include <mutex>
+// std::mutex mtx;
+// void Show(char ch)
+// {
+//     mtx.lock();
+//     for (int i = 0; i < 5; i++)
+//     {
+//         for (int j = 0; j < 10; j++)
+//         {
+//             std::cout << ch;
+//             std::this_thread::sleep_for(std::chrono::milliseconds(50));
+//         }
+//         std::cout << std::endl;
+//     }
+//     std::cout << std::endl;
+//     mtx.unlock();
+// }
+// int main()
+// {
+//     std::thread t1(Show, '*');
+//     std::thread t2(Show, '#');
+//     t1.join();
+//     t2.join();
+//     return 0;
+// }
 
-#include <iostream>
-#include <thread>
-#include <chrono>
-#include <mutex>
-std::mutex mtx;
-void Show(char ch)
-{
-    mtx.lock();
-    for (int i = 0; i < 5; i++)
-    {
-        for (int j = 0; j < 10; j++)
-        {
-            std::cout << ch;
-            std::this_thread::sleep_for(std::chrono::milliseconds(50));
-        }
-        std::cout << std::endl;
-    }
-    std::cout << std::endl;
-    mtx.unlock();
-}
 int main()
 {
-    std::thread t1(Show, '*');
-    std::thread t2(Show, '#');
-    t1.join();
-    t2.join();
+    
     return 0;
 }
