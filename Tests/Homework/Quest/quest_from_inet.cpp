09a8fb02bce9290d408711e89e96f896b79733da
MykhailenkoAnton <mykhailencko2594@gmail.com>
2022-07-31 19:16:31 +0300

    homework

--- Tests/Homework/Quest/quest_from_inet.cpp
+++ Tests/Homework/Quest/quest_from_inet.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+class A
+{
+public:
+A() {}
+~A(){}
+void Show() {std::cout << "A\n";}
+};
+class B : public A
 
+{
+public:
+    B() {}
+    ~B() {}
+    void Show() {std::cout << "B\n";}
+};
+
+
+
+void Foo(A b);
 int main()
 {
-    
+    A b;
+    Foo(b);
     return 0;
+}
+
+void Foo(A b)
+{
+    b.Show();
 }
